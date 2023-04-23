1f9e61d5278a3f89d5f8b497db904649ef2f166d
Gareth Aguilar <garethaguilar@gmail.com>
2023-04-23 12:42:43 -0500

    Add initial project files

---
+++ Source/GenGame/GenGame.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GenGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GenGame, "GenGame" );
+ 
