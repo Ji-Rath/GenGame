6e68679921dbcaa2d50376064ef72b2a8a44558e
Gareth Aguilar <garethaguilar@gmail.com>
2023-07-16 12:17:43 -0500

    Experimental changes

---
+++ Source/GenGame/Player/GenPlayerController.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GenPlayerController.h"
+
+
+// Sets default values
+AGenPlayerController::AGenPlayerController()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AGenPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGenPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AGenPlayerController::BeginSpectating()
+{
+	BeginSpectatingState();
+}
+
---
+++ Source/GenGame/Player/GenPlayerState.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GenPlayerState.h"
+
+
+// Sets default values
+AGenPlayerState::AGenPlayerState()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AGenPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGenPlayerState::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
